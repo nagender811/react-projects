710373fd2adfa1298be9b941a401a8c7965b8c9f
nagender811 <kumarnagender811@gmail.com>
2024-10-18 15:41:43 +0530

    update tailwind.conig

--- 02_basicProjects/tailwind.config.js
+++ 02_basicProjects/tailwind.config.js
@@ -8,7 +8,16 @@ export default {
     extend: {
       boxShadow: {
         'customShadow' : 'rgb(38, 57, 77) 0px 20px 30px -10px'
-      }
+      },
+      keyframes: {
+        animateModal: {
+          '0%': { top: '-200px', opacity: '0' },
+          '100%': { top: '0', opacity: '1' },
+        },
+      },
+      animation: {
+        'modal-in': 'animateModal 0.5s forwards',
+      },
     },
   },
   plugins: [],
